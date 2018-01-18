47480651b6e452d182cd544e8f246f075ed247e3
Hoang Pham <huyhoang8a5@gmail.com>
2018-01-19 00:44:45 +0800

    Add Firebase integration

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueFire from 'vuefire';
+import firebase from 'firebase/app';
+import * as auth from 'firebase/auth';
+import * as db from 'firebase/database';
 
 import store from './store';
 import "./styles/app.scss";
 
-console.log('store', store);
+import * as config from './assets/firebase-config.json';
+firebase.initializeApp(config);
+
+Vue.use(VueFire);
 new Vue({
-  el: '#app',
-  store,
-  render: h => h(App)
+    el: '#app',
+    store,
+    render: h => h(App)
 });
