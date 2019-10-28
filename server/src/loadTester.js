012c9b09e95bbb17f8d99a1f16cb431f34aa5f80
Hoang Pham <huyhoang8a5@gmail.com>
2019-10-28 13:14:45 +0800

    Add max-bet functionality

--- server/src/loadTester.js
+++ server/src/loadTester.js
@@ -2,46 +2,46 @@
 const mockData = require('./../config/mockdata');
 
 type Emitter = {
-    emit(name: string, event: Object): void
+  emit(name: string, event: Object): void
 };
 class LoadTester {
-    emitter: Emitter;
+  emitter: Emitter;
 
-    constructor(emitter: Emitter) {
-        this.emitter = emitter;
+  constructor(emitter: Emitter) {
+    this.emitter = emitter;
+  }
+
+  getRandomInt(min: number, max: number): number {
+    return min + Math.floor(Math.random() * Math.floor(max - min + 1));
+  }
+
+  getRandomUser() {
+    return mockData[this.getRandomInt(0, mockData.length - 1)];
+  }
+
+  wait(miliseconds: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, miliseconds));
+  }
+
+  async runLoadTest(numberOfUser: number) {
+    const players = [];
+    for (let index = 0; index < numberOfUser; index++) {
+      const user = this.getRandomUser();
+      const player = {
+        id: user.id,
+        name: user.name,
+        avatar: user.avatar,
+        bet: this.getRandomInt(1, 30),
+        choice: this.getRandomInt(1, 6)
+      };
+      players.push(player);
     }
 
-    getRandomInt(min: number, max: number): number {
-        return min + Math.floor(Math.random() * Math.floor(max - min + 1));
+    for (const player of players) {
+      await this.wait(50);
+      this.emitter.emit('newBet', player);
     }
-
-    getRandomUser() {
-        return mockData[this.getRandomInt(0, mockData.length - 1)];
-    }
-
-    wait(miliseconds: number): Promise <void> {
-        return new Promise((resolve) => setTimeout(resolve, miliseconds));
-    }
-
-    async runLoadTest(numberOfUser: number) {
-        const players = [];
-        for (let index = 0; index < numberOfUser; index++) {
-            const user = this.getRandomUser();
-            const player = {
-                id: user.id,
-                name: user.name,
-                avatar: user.avatar,
-                bet: this.getRandomInt(1, 4),
-                choice: this.getRandomInt(1, 6)
-            };
-            players.push(player);
-        }
-
-        for (const player of players) {
-            await this.wait(50);
-            this.emitter.emit('newBet', player);
-        }
-    }
+  }
 }
 
 module.exports = LoadTester;
